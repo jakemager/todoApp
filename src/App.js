904a9605bcdfe9ab454114201e0e360b86b6b196
Jake Mager <jmager12@gmail.com>
2019-03-24 20:11:47 -0400

    Install packages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './App.scss';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<div>Categories</div>
+				<div>Todo task</div>
+				<div>task info</div>
+			</div>
+		);
+	}
 }
 
 export default App;
